8138a44dcb25783c53c88278cd632343a427e4b0
esnoko <elvisnoko18@gmail.com>
2025-02-05 00:18:00 +0200

    Add button

---
+++ components/Hero.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React from 'react';
+import Image from 'next/image';
+import CustomButton from './CustomButton';
+
+const hero = () => {
+  const handleScroll = () => {
+    
+  }
+  return (
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'>
+        <h1 className='hero__title'>Locate, book, and drive – anytime, anywhere.</h1>
+        <p className='hero__subtitle'>Revolutionize your travel plans with our simple booking solution.</p>
+
+        <CustomButton
+           title = "Book Now"
+           containerStyles = "bg-primary-blue text-white rounded-full mt-8"
+           handleClick = {handleScroll}
+        />
+      </div>
+
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default hero
