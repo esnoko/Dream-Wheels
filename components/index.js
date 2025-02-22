47f0feb2e10ede64293f1ed94b44ace3e806a8b2
esnoko <elvisnoko18@gmail.com>
2025-02-22 22:50:46 +0200

    Add Navbar and Footer files

--- components/index.js
+++ components/index.js
@@ -1,7 +1,6 @@
-import Hero from './Hero'
-import CustomButton from './CustomButton'
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
 
-export{
-    Hero,
-    CustomButton
-}
+export { Navbar, Hero, CustomButton, Footer, };
