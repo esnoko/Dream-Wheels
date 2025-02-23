ea51c97e69c9f4f8f34bd8090e9d96046f1718c7
esnoko <elvisnoko18@gmail.com>
2025-02-23 14:36:28 +0200

    Update Footer and Hero files

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,34 +1,40 @@
 "use client";
-import React from 'react';
-import Image from 'next/image';
-import CustomButton from './CustomButton';
+import React from "react";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const hero = () => {
-  const handleScroll = () => {
-    
-  }
+  const handleScroll = () => {};
   return (
-    <div className='hero'>
-      <div className='flex-1 pt-36 padding-x'>
-        <h1 className='hero__title'>Locate, book, and drive – anytime, anywhere.</h1>
-        <p className='hero__subtitle'>Revolutionize your travel plans with our simple booking solution.</p>
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Locate, book, and drive – anytime, anywhere.
+        </h1>
+        <p className="hero__subtitle">
+          Revolutionize your travel plans with our simple booking solution.
+        </p>
 
         <CustomButton
-           title = "Book Now"
-           containerStyles = "bg-primary-blue text-white rounded-full mt-8"
-           handleClick = {handleScroll}
+          title="Book Now"
+          containerStyles="bg-primary-blue text-white rounded-full mt-8"
+          handleClick={handleScroll}
         />
       </div>
 
-      <div className='hero__image-container'>
-        <div className='hero__image'>
-          <Image src="/hero.png" alt="hero image" fill className="object-contain"/>
-
-          <div className='hero__image-overlay'/>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image
+            src="/hero.png"
+            alt="hero image"
+            fill
+            className="object-contain"
+          />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default hero
+export default hero;
