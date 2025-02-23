ce0e926b4676500f4a7a56e1b5d6dd6cb10d8bb0
esnoko <elvisnoko18@gmail.com>
2025-02-23 14:51:52 +0200

    Add CustomFilter and SearchBar files

--- components/index.js
+++ components/index.js
@@ -2,5 +2,7 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
 
-export { Navbar, Hero, CustomButton, Footer, };
+export { Navbar, Hero, CustomButton, Footer, SearchBar, CustomFilter };
