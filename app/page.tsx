7217db4e9f7fc13064da7c2c226ea507b87fc18b
esnoko <elvisnoko18@gmail.com>
2025-02-26 01:53:10 +0200

    Update CarDetails and SearchBar files

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
 import { fetchCars } from "@/utils";
 import { HomeProps } from "@/types";
 import { fuels, yearsOfProduction } from "@/constants";
-import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@/components";
+import { CarCard, SearchBar, CustomFilter, Hero } from "@/components";
 
 export default async function Home({ searchParams }: HomeProps) {
-  const allCars = await fetchCars({
-    manufacturer: searchParams.manufacturer || "",
-    year: searchParams.year || 2022,
-    fuel: searchParams.fuel || "",
-    limit: searchParams.limit || 10,
-    model: searchParams.model || "",
-  });
+  const allCars = await fetchCars();
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
@@ -41,10 +35,6 @@ export default async function Home({ searchParams }: HomeProps) {
               ))}
             </div>
 
-            <ShowMore
-              pageNumber={(searchParams.limit || 10) / 10}
-              isNext={(searchParams.limit || 10) > allCars.length}
-            />
           </section>
         ) : (
           <div className='home__error-container'>
