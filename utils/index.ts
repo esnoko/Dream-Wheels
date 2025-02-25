12972d4fbdd981d206f802b8e9986582ecb0e5e5
esnoko <elvisnoko18@gmail.com>
2025-02-25 22:02:02 +0200

    Add utils

---
+++ utils/index.ts
@@ -0,0 +1,22 @@
+export async function fetchCars() {
+    // Set the required headers for the API request
+    const headers: HeadersInit = {
+      'x-rapidapi-key': 'efe6b207cbmsh1cf92dbff5c67b3p1c5bf4jsn5ae74c9f84c8',
+      'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+    };
+  
+    // Set the required headers for the API request
+    const response = await fetch(
+      `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars`,
+      {
+        headers: headers,
+      }
+    );
+  
+    // Parse the response as JSON
+    const result = await response.json();
+  
+    return result;
+  }
+
+  
