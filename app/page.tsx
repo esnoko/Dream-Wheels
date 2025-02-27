663c6d1a815731dd9ecee8c9934f3962e4f262de
esnoko <elvisnoko18@gmail.com>
2025-02-28 00:42:02 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,9 @@
-import { fetchCars } from "@/utils";
-import { HomeProps } from "@/types";
-import { fuels, yearsOfProduction } from "@/constants";
-import { Hero, CarCard, SearchBar, CustomFilter} from "@/components";
+import React from 'react'
 
-export default async function Home({ searchParams }: HomeProps) {
-  const allCars = await fetchCars();
-
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
-
+function page() {
   return (
-    <main className='overflow-hidden'>
-      <Hero />
-
-      <div className='mt-12 padding-x padding-y max-width' id='discover'>
-        <div className='home__text-container'>
-          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
-          <p>Explore out cars you might like</p>
-        </div>
-
-        <div className='home__filters'>
-          <SearchBar />
-
-          <div className='home__filter-container'>
-            <CustomFilter title='fuel' options={fuels} />
-            <CustomFilter title='year' options={yearsOfProduction} />
-          </div>
-        </div>
-
-        {!isDataEmpty ? (
-          <section>
-            <div className='home__cars-wrapper'>
-              {allCars?.map((car) => (
-                <CarCard car={car} />
-              ))}
-            </div>
-          </section>
-        ) : (
-          <div className='home__error-container'>
-            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
-            <p>{allCars?.message}</p>
-          </div>
-        )}
-      </div>
-    </main>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
