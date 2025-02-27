dfb06b10d6c0e51178237b8bb033435386c65b75
esnoko <elvisnoko18@gmail.com>
2025-02-27 23:54:37 +0200

    Update

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useState, Fragment } from "react";
 import { manufacturers } from "@/constants";
 import { SearchManuFacturerProps } from "@/types";
-import { Combobox, Transition, } from "@headlessui/react";
+import { Combobox, Transition } from "@headlessui/react";
 
 const SearchManufacturer = ({
   manufacturer,
@@ -11,14 +11,14 @@ const SearchManufacturer = ({
 }: SearchManuFacturerProps) => {
   const [query, setQuery] = useState("");
   const filteredManufacturers =
-  query === ""
-    ? manufacturers
-    : manufacturers.filter((item) =>
-        item
-          .toLowerCase()
-          .replace(/\s+/g, "")
-          .includes(query.toLowerCase().replace(/\s+/g, ""))
-      );
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
   return (
     <div className="search-manufacturer">
       <Combobox>
@@ -39,19 +39,19 @@ const SearchManufacturer = ({
           {/* Transition for displaying the options */}
           <Transition
             as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
-            leave='transition ease-in duration-100'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
             afterLeave={() => setQuery("")} // Reset the search query after the transition completes
           >
             <Combobox.Options
-              className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
+              className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
               static
             >
               {filteredManufacturers.length === 0 && query !== "" ? (
                 <Combobox.Option
                   value={query}
-                  className='search-manufacturer__option'
+                  className="search-manufacturer__option"
                 >
                   Create "{query}"
                 </Combobox.Option>
@@ -68,13 +68,22 @@ const SearchManufacturer = ({
                   >
                     {({ selected, active }) => (
                       <>
-                        <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
+                        <span
+                          className={`block truncate ${
+                            selected ? "font-medium" : "font-normal"
+                          }`}
+                        >
                           {item}
                         </span>
 
                         {/* Show an active blue background color if the option is selected */}
                         {selected ? (
-                          <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active
+                                ? "text-white"
+                                : "text-pribg-primary-purple"
+                            }`}
                           ></span>
                         ) : null}
                       </>
