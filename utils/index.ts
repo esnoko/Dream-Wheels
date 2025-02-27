c7f98f3c8eb691dc39000ce36a3db90285badc41
esnoko <elvisnoko18@gmail.com>
2025-02-28 00:47:59 +0200

    Update

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,7 @@
 import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
   // Set the required headers for the API request
   const headers: HeadersInit = {
     "X-RapidAPI-Key": "efe6b207cbmsh1cf92dbff5c67b3p1c5bf4jsn5ae74c9f84c8",
@@ -9,7 +10,7 @@ export async function fetchCars() {
 
   // Set the required headers for the API request
   const response = await fetch(
-    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3`,
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     }
